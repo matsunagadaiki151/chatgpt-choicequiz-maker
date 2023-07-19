6a17762057951a00652c555d47aabc564f30f0d9
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-19 20:37:15 +0900

    クラスをスキーマに移動

---
+++ backend/src/schema/quizzes_schema.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class Choice(BaseModel):
+    choice_id: int = Field(description="choice_id")
+    choice_sentence: str = Field(description="choice description")
+
+
+class Quiz(BaseModel):
+    quiz_id: int = Field(description="quiz_id")
+    question: str = Field(description="quiz sentence")
+    choice: List[Choice] = Field("choices")
+    answer_id: int = Field("correct choice id")
+
+
+class Quizzes(BaseModel):
+    Items: List[Quiz] = Field(description="quiz items")
