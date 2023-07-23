4c4c58c382c154093453946d1c9c8c214fd28840
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-23 21:59:35 +0900

    構造を調整

--- frontend/components/Question.tsx
+++ frontend/components/Question.tsx
@@ -3,9 +3,8 @@ import React from "react";
 
 const Question = ({ questionId, questionSentent }: TQuestion) => {
   return (
-    <div className="text-giray m-2 font-marugo">
-      <p className="font-bold mb-1">{`問題${questionId}`}</p>
-      <div>{questionSentent}</div>
+    <div className="text-giray font-marugo">
+      <p className="font-bold mb-1">{`問題${questionId} : ${questionSentent}`}</p>
     </div>
   );
 };
