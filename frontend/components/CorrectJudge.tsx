817f040ef68d04728d3aa1ea9aa38df40b30002b
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-23 16:34:16 +0900

    スタイルを微調整

--- frontend/components/CorrectJudge.tsx
+++ frontend/components/CorrectJudge.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 
 const CorrectJudge = ({ isCorrect }: TCorrectJugde) => {
   return (
-    <div className="text-3xl font-marugo font-bold" role="judge">
+    <div className="text-3xl font-marugo font-bold p-2 w-36 text-center">
       {isCorrect ? (
         <p className="text-red-500">正解！</p>
       ) : (
