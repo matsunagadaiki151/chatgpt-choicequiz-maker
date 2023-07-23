605d5256eb727af6a500e71d7674e6f74b8f3630
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-23 22:00:54 +0900

    サンプルのオブジェクトを利用するよう修正

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,36 @@
-import Question from "@/components/Question";
-import Image from "next/image";
+"use client";
 
-const questionSentent = "これはなんでしょう?";
+import QuizLayout from "@/layouts/QuizLayout";
+
+const sampleQuiz = {
+  quiz_id: 1,
+  question: "LangChainは何のためのライブラリですか？",
+  choice: [
+    {
+      choice_id: 1,
+      choice_sentence: "大規模言語モデルを使ったサービス開発に役立つ",
+    },
+    {
+      choice_id: 2,
+      choice_sentence: "データベースを操作するためのモジュール",
+    },
+    {
+      choice_id: 3,
+      choice_sentence: "画像処理を行うためのライブラリ",
+    },
+    {
+      choice_id: 4,
+      choice_sentence: "音声認識を行うためのモジュール",
+    },
+  ],
+  answer_id: 1,
+};
+
 export default function Home() {
   return (
     <main>
       <div className="text-center">
-        <Question questionId={1} questionSentent={questionSentent}></Question>
+        <QuizLayout quiz={sampleQuiz} />
       </div>
     </main>
   );
