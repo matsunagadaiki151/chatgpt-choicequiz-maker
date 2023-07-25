122b12cddd7443b946e822c5dafe9a19cf0ed124
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-25 20:06:58 +0900

    フッターを作成

---
+++ frontend/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { TFooter } from "@/types/componentTypes";
+import Link from "next/link";
+
+/**
+ * Primary UI component for user interaction
+ */
+const Footer = ({ author, createYear }: TFooter) => {
+  return (
+    <footer className="w-full bg-giray text-center items-center pt-6 pb-2 fixed bottom-0 left-0">
+      <div className="font-marugo text-white text-xs">
+        {`${author} @${createYear}`}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
