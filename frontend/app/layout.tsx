c51f9a95d85c705c40061a399893e7e3f5b4275d
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-25 20:29:25 +0900

    レイアウトを作成

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,23 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Header from "@/components/Header/Header";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer/Footer";
+import {
+  author,
+  createYear,
+  siteTitle,
+} from "@/features/quiz/const/layoutConstants";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'クイズメーカー',
-  description: '技術記事からクイズを作成するよ!!',
-}
+  title: "クイズメーカー",
+  description: "技術記事からクイズを作成するよ!!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+        <Header title={siteTitle} />
+        {children}
+        <Footer author={author} createYear={createYear} />
       </body>
     </html>
-  )
+  );
 }
---
+++ frontend/features/quiz/const/layoutConstants.ts
@@ -0,0 +1,3 @@
+export const siteTitle = "AI Mondai Maker";
+export const author = "giratro";
+export const createYear = 2023;
