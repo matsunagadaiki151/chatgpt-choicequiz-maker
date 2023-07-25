8f3701fb18b3f039d8be61059f742560d8d9f8a2
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-25 21:43:29 +0900

    「次の問題へ」リンクの追加

--- frontend/app/quiz/[id]/page.tsx
+++ frontend/app/quiz/[id]/page.tsx
@@ -1,12 +1,13 @@
 import { getMockQuiz } from "@/features/quiz/api/getQuiz";
 import QuizLayout from "@/features/quiz/components/Quiz";
 import { TQuizPageParam } from "@/types/pageParam";
+import Link from "next/link";
 
-export default async function Home({ params }: TQuizPageParam) {
+export default async function Page({ params }: TQuizPageParam) {
   const { id } = params;
   const sampleQuizzes = await getMockQuiz();
   if (id >= 6) {
-    return <div>idは5以内でお願いします。</div>;
+    return <div>Not Found</div>;
   }
   const sampleQuiz = sampleQuizzes[id - 1];
   return (
@@ -14,6 +15,19 @@ export default async function Home({ params }: TQuizPageParam) {
       <div className="text-center">
         <QuizLayout quiz={sampleQuiz} />
       </div>
+      {id < 5 ? (
+        <Link href={`/quiz/${Number(id) + 1}`}>
+          <div className="mt-20 text-right mr-24 font-marugo text-giray hover:text-blue-300">
+            次の問題へ→
+          </div>
+        </Link>
+      ) : (
+        <Link href="/result">
+          <div className="mt-20 text-right mr-24 font-marugo text-giray hover:text-blue-300">
+            問題を終了する
+          </div>
+        </Link>
+      )}
     </main>
   );
 }
