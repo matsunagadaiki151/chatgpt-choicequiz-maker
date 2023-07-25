0aff3dfb6f33bf4f510d1ceeb0230aaa05d210b0
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-25 21:43:39 +0900

    リザルト画面の作成

---
+++ frontend/app/result/page.tsx
@@ -0,0 +1,12 @@
+import QuizLayout from "@/features/quiz/components/Quiz";
+import { TQuizPageParam } from "@/types/pageParam";
+
+export default async function Page() {
+  return (
+    <main>
+      <div className="text-3xl text-center font-marugo text-giray">
+        よく頑張りました。
+      </div>
+    </main>
+  );
+}
