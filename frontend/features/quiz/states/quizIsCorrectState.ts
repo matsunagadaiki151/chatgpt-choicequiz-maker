6b12d6d89a39a1e242e27f65494f01f2d42421c4
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-07-31 07:25:16 +0900

    recoil-persistによる値の永続化を実装

--- frontend/features/quiz/states/quizIsCorrectState.ts
+++ frontend/features/quiz/states/quizIsCorrectState.ts
@@ -1,8 +1,12 @@
 import { atom, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const quizIsCorrectState = atom<boolean[]>({
   key: "quizIsCorrectState",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const quizIsCorrectSelector = selector<number>({
