3fd8f6d073a4092aaea835f6afcaa95f259520df
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-01 21:01:34 +0900

    トップへ戻るボタンを追加

--- frontend/app/result/page.tsx
+++ frontend/app/result/page.tsx
@@ -1,12 +1,17 @@
-import QuizLayout from "@/features/quiz/components/Quiz";
+import LinkButton from "@/components/LinkButton/LinkButton";
 import Result from "@/features/quiz/components/Result";
 
 export default async function Page() {
   return (
-    <main>
-      <div className="text-3xl text-center font-marugo text-giray">
+    <div className="h-full flex flex-col items-center">
+      <div className="text-3xl text-center font-marugo text-giray flex-1">
         <Result />
       </div>
-    </main>
+      <div className="mb-20">
+        <LinkButton bgColor="blue" href="/">
+          トップへ戻る
+        </LinkButton>
+      </div>
+    </div>
   );
 }
