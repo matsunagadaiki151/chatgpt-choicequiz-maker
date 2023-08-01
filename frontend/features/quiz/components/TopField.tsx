dec676f71d4b0fd8bbddd6bfaafb2b4c61eec656
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-01 21:02:09 +0900

    トップページを改修

--- frontend/features/quiz/components/TopField.tsx
+++ frontend/features/quiz/components/TopField.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import Button from "@/components/Button/Button";
+import SentenceField from "@/components/SententceField/SentenceField";
 import { quizListState } from "@/features/quiz/states/quizListState";
 import { TQuiz } from "@/features/quiz/types/QuizType";
 import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
 import { useSetRecoilState } from "recoil";
 
 type TQuizListProps = {
@@ -12,16 +14,32 @@ type TQuizListProps = {
 
 const TopField = ({ quizzes }: TQuizListProps) => {
   const router = useRouter();
+  const [sentence, SetSentence] = useState("");
   const setQuizList = useSetRecoilState(quizListState);
   const topButtonClick = async () => {
     setQuizList(quizzes);
     router.push("/quiz/1");
   };
 
+  const onSentenceChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    SetSentence(e.target.value);
+  };
+
   return (
-    <Button bgColor="blue" onButtonClick={topButtonClick}>
-      クイズを解く
-    </Button>
+    <>
+      <div className="flex flex-col space-y-10 items-center">
+        <div className="text-4xl text-bold font-marugo text-giray">
+          問題を作ろう!!
+        </div>
+        <SentenceField
+          sentence={sentence}
+          onSentenceChange={onSentenceChange}
+        />
+        <Button bgColor="blue" onButtonClick={topButtonClick}>
+          問題を作成する
+        </Button>
+      </div>
+    </>
   );
 };
 
