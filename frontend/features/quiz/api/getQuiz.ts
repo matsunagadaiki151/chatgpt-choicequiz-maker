02100ce34b4129e2c5baf19ea4485c9648c3d851
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-07 08:37:38 +0900

    型指定の方法を微修正

--- frontend/features/quiz/api/getQuiz.ts
+++ frontend/features/quiz/api/getQuiz.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { TQuiz } from "../types/QuizType";
+import { sleep } from "../hooks/timer";
 
 export const getMockQuiz = async () => {
   const url = "http://localhost:8001/Items";
-  const res = await axios.get(url);
+  const res = await axios.get<TQuiz[]>(url);
 
-  const data: TQuiz[] = res.data;
+  await sleep(2000);
+
+  const data = res.data;
   return data;
 };
