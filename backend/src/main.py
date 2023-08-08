cad7c12ad58f939ddd1d673eb136ebf851173bd0
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-08 12:41:53 +0900

    CORSを設定

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import create
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(create.router)
