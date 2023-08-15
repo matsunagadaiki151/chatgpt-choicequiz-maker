3c714b81ae333cbf5b3ec5ee533e9a603cbe685a
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-15 22:19:43 +0900

    3000文字以上の時は、map_reduceを行うよう修正

--- backend/src/services/openai_service.py
+++ backend/src/services/openai_service.py
@@ -1,7 +1,10 @@
 from langchain import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
+from langchain.docstore.document import Document
 from langchain.output_parsers import PydanticOutputParser
+from langchain.text_splitter import CharacterTextSplitter
 
 from schema.quizzes_schema import Quizzes
 
@@ -14,7 +17,7 @@ def load_text(text_path: str):
         return f.read()
 
 
-def create_quiz(text: str):
+def create_quiz_normal(text: str):
     template = """以下の説明文から日本語の文章の4択クイズを5つ作成し特定のフォーマットで出力してください。
 誤った選択肢は存在しない単語や、反対の意味を持つ文章等明らかに間違ったものになるようにしてください。
 
@@ -39,3 +42,63 @@ def create_quiz(text: str):
     quizzes = parser.parse(output)
 
     return quizzes
+
+
+def create_quiz_map_reduce(text: str):
+    # テキストの分割を行う。
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(text)
+    docs = [Document(page_content=t) for t in texts]
+
+    # 要約を行う。
+    map_template = """
+    以下の内容について1000文字程度の要約を書いて下さい。
+    ただし、文字数が少ない場合は可能な範囲で要約を書いてください。
+
+    "{text}"
+
+    要約:"""
+
+    map_prompt = PromptTemplate(
+        template=map_template, input_variables=["text"]
+    )
+    summary_chain = load_summarize_chain(
+        llm=chat,
+        chain_type="map_reduce",
+        map_prompt=map_prompt,
+        combine_prompt=map_prompt,
+    )
+    template = """以下の説明文から4択クイズを5つ作成し特定のフォーマットで出力してください。
+    クイズは必ず日本語で作成してください。
+    また誤った選択肢は存在しない単語や、反対の意味を持つ文章等明らかに間違ったものになるようにしてください。
+
+    説明文: {document}
+
+    {format_instructions}
+    """
+
+    parser = PydanticOutputParser(pydantic_object=Quizzes)
+
+    prompt_subject = PromptTemplate(
+        template=template,
+        input_variables=["document"],
+        partial_variables={
+            "format_instructions": parser.get_format_instructions()
+        },
+    )
+    chain_subject = LLMChain(llm=chat, prompt=prompt_subject)
+    overall_chain_map_reduce = SimpleSequentialChain(
+        chains=[summary_chain, chain_subject]
+    )
+    output = overall_chain_map_reduce.run(docs)
+
+    quizzes = parser.parse(output)
+
+    return quizzes
+
+
+def create_quiz(text: str):
+    if len(text) < 3500:
+        return create_quiz_normal(text)
+    else:
+        return create_quiz_map_reduce(text)
