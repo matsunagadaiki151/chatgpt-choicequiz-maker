f91cf3c172893364a2229421e6073775e7c557db
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-28 21:37:28 +0900

    自分の選択をRecoilで管理するよう修正

--- frontend/features/quiz/components/TopLoading/TopLoading.tsx
+++ frontend/features/quiz/components/TopLoading/TopLoading.tsx
@@ -2,18 +2,14 @@
 
 import useSWR from "swr";
 import { getQuizFromSentence } from "../../api/getQuiz";
-import {
-  useRecoilState,
-  useRecoilValue,
-  useSetRecoilState,
-  waitForAllSettled,
-} from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { quizListState } from "../../states/quizListState";
 import { quizIsCorrectState } from "../../states/quizIsCorrectState";
 import { useEffect, useState } from "react";
 import { TLoading, TModelName } from "../../types/QuizType";
 import { modelNameState } from "../../states/modelNameState";
 import { isLoadingState } from "../../states/isLoadingState";
+import { selectedOptionsState } from "../../states/selectedOptionsState";
 
 const fetcher = (url: string, sentence: string, modelName: TModelName) => {
   const quizzes = getQuizFromSentence(url, sentence, modelName);
@@ -23,6 +19,7 @@ const fetcher = (url: string, sentence: string, modelName: TModelName) => {
 const TopLoading = ({ sentence }: TLoading) => {
   const [is502Error, setIs502Error] = useState<boolean>(false);
   const setIsLoading = useSetRecoilState(isLoadingState);
+  const setSelectedOptions = useSetRecoilState(selectedOptionsState);
 
   useEffect(() => {
     setIs502Error(false);
@@ -51,6 +48,7 @@ const TopLoading = ({ sentence }: TLoading) => {
     if (data !== undefined) {
       setQuizzes(data);
       setQuizIsCorrects(Array(QUIZ_NUM).fill(false));
+      setSelectedOptions(Array(QUIZ_NUM).fill(""));
       setIsLoading(false);
     }
   }, []);
