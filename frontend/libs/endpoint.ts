5dab2a23c968a5dffda1abe7d0d3f111cdc89e50
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-09-05 23:35:59 +0900

    環境変数が読み込めなかったときにExceptionを投げるよう修正

--- frontend/libs/endpoint.ts
+++ frontend/libs/endpoint.ts
@@ -1,3 +1,5 @@
-const getEndpoint = {};
 export const endpoint = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT ?? "";
-console.log(endpoint);
+if (endpoint === "") {
+  throw new Error("環境変数の読み込みに失敗しました。");
+}
+console.log("endpoint: ", endpoint);
