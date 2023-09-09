20b413220017af7c7e3ec361b694544b762942e5
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-09-09 14:09:31 +0900

    16kでの推奨文字列が3000になってたので修正

--- backend/src/services/gpt_service.py
+++ backend/src/services/gpt_service.py
@@ -11,12 +11,10 @@ from schema.quizzes_schema import Quizzes
 
 
 class GPTService:
-    def __init__(self, text, model_name):
+    def __init__(self, text, model_name, recommend_length):
         self.llm = ChatOpenAI(model=model_name, temperature=0, client=None)
-
-        self.recommend_length = 3000
         self.parser = PydanticOutputParser(pydantic_object=Quizzes)
-
+        self.recommend_length = recommend_length
         self.text = text
         self.template = """以下の説明文から日本語の文章の4択クイズを5つ作成し特定のフォーマットで出力してください。
         誤った選択肢は存在しない単語や、反対の意味を持つ文章等明らかに間違ったものになるようにしてください。
@@ -28,7 +26,11 @@ class GPTService:
 
     @staticmethod
     def from_model_name(text, model_name):
-        return GPTService(text, model_name)
+        model_length_dic = {
+            "gpt-3.5-turbo": 3000,
+            "gpt-3.5-turbo-16k": 12000,
+        }
+        return GPTService(text, model_name, model_length_dic[model_name])
 
     def create_quiz_normal(self):
         prompt = PromptTemplate(
@@ -44,12 +46,28 @@ class GPTService:
         output = chain.run(self.text)
         return output
 
+    def insert_text_br(self):
+        def insert_string_at_position(input_string, position, insert_text):
+            # n番目の位置に文字列を挿入する
+            return (
+                input_string[:position] + insert_text + input_string[position:]
+            )
+
+        new_text = self.text
+        for i in range(1, len(self.text) // self.recommend_length):
+            new_text = insert_string_at_position(
+                new_text, i * self.recommend_length, "<br>"
+            )
+        return new_text
+
     def create_quiz_map_reduce(self):
+        # textに一定間隔で<br>を埋め込む
+        processed_text = self.insert_text_br()
         # テキストの分割を行う。
         text_splitter = CharacterTextSplitter(
-            separator="<br>", chunk_size=3000
+            separator="<br>", chunk_size=self.recommend_length
         )
-        texts = text_splitter.split_text(self.text)
+        texts = text_splitter.split_text(processed_text)
         docs = [Document(page_content=t) for t in texts]
 
         # 要約を行う。
@@ -86,7 +104,7 @@ class GPTService:
 
         return output
 
-    def parse_output(self, output: str):
+    def _parse_output(self, output: str):
         try:
             quizzes = self.parser.parse(output)
             if len(quizzes.Items) != 5:
@@ -100,4 +118,4 @@ class GPTService:
             output = self.create_quiz_normal()
         else:
             output = self.create_quiz_map_reduce()
-        return self.parse_output(output)
+        return self._parse_output(output)
