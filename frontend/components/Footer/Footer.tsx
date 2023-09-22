45af33fdcb0baf677c9d1e9b7455578d697344b5
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-09-22 18:51:46 +0900

    マージンを修正

--- frontend/components/Footer/Footer.tsx
+++ frontend/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
  */
 const Footer = ({ author, createYear }: TFooter) => {
   return (
-    <footer className="w-full bg-giray flex justify-between text-center items-center py-5 text-gray-100 font-marugo">
+    <footer className="w-full bg-giray flex justify-between text-center items-center mt-20 py-5 text-gray-100 font-marugo">
       <div className="ml-20">{`${author} @${createYear}`}</div>
       <div className="flex justify-center mr-20 space-x-6 ">
         <Link href="/term-of-service" className="hover:text-orange-300">
