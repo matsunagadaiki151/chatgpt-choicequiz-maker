699ac1d72def30965c6a79877a6c4303d2b1cd3a
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-09-27 22:59:09 +0900

    回答の詳細が表示されるように修正

---
+++ frontend/app/result/detail/page.tsx
@@ -0,0 +1,15 @@
+import LinkButton from "@/components/LinkButton/LinkButton";
+import Detail from "@/features/result/components/Detail";
+
+export default async function Page() {
+  return (
+    <div className="h-full flex flex-col items-center justify-between text-giray font-marugo">
+      <Detail />
+      <div className="my-20">
+        <LinkButton bgColor="blue" size="medium" href="/result">
+          結果画面へ戻る
+        </LinkButton>
+      </div>
+    </div>
+  );
+}
