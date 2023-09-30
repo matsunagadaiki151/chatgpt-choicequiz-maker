a15daf903e39a1fe26a007ea432266369ad14919
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-09-30 15:39:05 +0900

    利用状況に応じて使用できなくなることを明示

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,12 @@
 import TopField from "@/features/topPage/components/TopField/TopField";
 
 export default async function Home() {
-  return <TopField />;
+  return (
+    <>
+      <p className="text-center font-marugo font-bold text-red-500 mb-10 text-xs md:text-base">
+        ※ 全体の利用状況に応じて使用できなくなる場合があります。
+      </p>
+      <TopField />
+    </>
+  );
 }
