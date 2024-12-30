29cf3153db173e4db60141d7343276c29932f9c1
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2024-12-30 20:15:14 +0900

    モデル固定化に伴うwarningの修正

--- frontend/features/topPage/components/Warning/Warning.tsx
+++ frontend/features/topPage/components/Warning/Warning.tsx
@@ -1,12 +1,16 @@
-import { TWarning } from "@/features/quiz/types/QuizType";
+// 現状はgpt-4o-miniで固定なので一旦定数として定義
+const SELECTED_MODEL = "gpt-4o-mini";
 
-const Warning = ({ sentence, selectedModelName }: TWarning) => {
+export type TWarning = {
+  sentence: string;
+};
+
+const Warning = ({ sentence }: TWarning) => {
   const recommendLengths: { [name: string]: number } = {
-    "gpt-3.5-turbo": 3000,
-    "gpt-3.5-turbo-16k": 12000,
+    "gpt-4o-mini": 12000,
   };
 
-  const sentenceLength = recommendLengths[selectedModelName];
+  const sentenceLength = recommendLengths[SELECTED_MODEL];
   return (
     <div className="text-yellow-500 text-center">
       {sentence.length > sentenceLength && (
@@ -19,13 +23,6 @@ const Warning = ({ sentence, selectedModelName }: TWarning) => {
           </div>
         </>
       )}
-      {selectedModelName === "gpt-3.5-turbo-16k" &&
-        sentence.length <= recommendLengths["gpt-3.5-turbo"] &&
-        sentence.length > 0 && (
-          <div>
-            {`文章が${recommendLengths["gpt-3.5-turbo"]}文字以内の場合GPT3.5Turboの使用を推奨します。`}
-          </div>
-        )}
     </div>
   );
 };
