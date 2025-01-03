4bf95b4bba4cceb753ef938c5253c9de9f59352a
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2025-01-03 15:43:52 +0900

    RecoilProviderを削除

--- frontend/app/provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ReactNode } from "react";
-import { RecoilRoot } from "recoil";
-
-export default function AppProvider({ children }: { children: ReactNode }) {
-  return <RecoilRoot>{children}</RecoilRoot>;
-}
