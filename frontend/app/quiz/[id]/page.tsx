94a6697743cfb89ecb2f0eb219e91dd5285f29c9
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2025-01-03 16:06:02 +0900

    codemodを適用

--- frontend/app/quiz/[id]/page.tsx
+++ frontend/app/quiz/[id]/page.tsx
@@ -1,12 +1,13 @@
 import QuizPage from "@/features/quiz/components/QuizPage/QuizPage";
 
 type PageProps = {
-  params: {
+  params: Promise<{
     id: number;
-  };
+  }>;
 };
 
-export default async function Page({ params }: PageProps) {
+export default async function Page(props: PageProps) {
+  const params = await props.params;
   const { id } = params;
 
   return <QuizPage id={id} />;
